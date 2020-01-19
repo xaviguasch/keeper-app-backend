04e377877864b6f945bfde50179c7e8332fffe30
xaviguasch <xaviguasch@gmail.com>
2020-01-19 13:57:53 +0100

    added Contact and ContactItem components

---
+++ client/src/components/contacts/ContactItem.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const ContactItem = ({ contact }) => {
+  const { name, id, email, phone, type } = contact
+
+  return (
+    <div className='card bg-light'>
+      <h3 className='text-primary text-left'>
+        {name}{' '}
+        <span
+          className={
+            'badge ' + (type === 'professional' ? 'badge-success' : 'badge-primary')
+          }
+        >
+          {type.charAt(0).toUpperCase() + type.slice(1)}
+        </span>
+      </h3>
+    </div>
+  )
+}
+
+export default ContactItem
